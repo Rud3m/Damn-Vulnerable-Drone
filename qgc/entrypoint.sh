33216b0fa486a542cce17c732b8fc7b5bc13a3e2
nicholasaleks <nicholas.aleks@gmail.com>
2025-07-04 11:41:33 -0400

    Removed old qgc container

--- qgc/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ "x${HEADLESS}" == "1" ]; then
-    echo -e "INFO\t[QGC] HEADLESS SET. RUNNING IN HEADLESS MODE."
-    xvfb-run /home/user/QGroundControl.AppImage
-else
-    /home/user/QGroundControl.AppImage
-fi
