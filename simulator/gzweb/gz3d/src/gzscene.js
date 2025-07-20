4e95662ccef0a342c6a38929eaa45e916f1f4144
nicholasaleks <nicholas.aleks@gmail.com>
2025-07-19 23:24:47 -0500

    Fixed Hi-DPI mouse coordinate mismatch

--- simulator/gzweb/gz3d/src/gzscene.js
+++ simulator/gzweb/gz3d/src/gzscene.js
@@ -41,7 +41,7 @@ GZ3D.Scene.prototype.init = function()
   this.stlLoader = new THREE.STLLoader();
 
   this.renderer = new THREE.WebGLRenderer({antialias: true });
-  this.renderer.setPixelRatio(window.devicePixelRatio);
+  this.renderer.setPixelRatio(1);
   this.renderer.setClearColor(0xb2b2b2, 1);
   this.renderer.autoClear = false;
   // this.renderer.shadowMapEnabled = true;
